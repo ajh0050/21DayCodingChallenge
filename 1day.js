c94f0107231446e4b5285ca151089ebc6765f408
Adam Hughes <adamjordanhughes@gmail.com>
2023-06-05 10:35:05 -0400

    First day challenge

---
+++ 1day.js
@@ -0,0 +1,13 @@
+
+/*
+  * You may assume the node class is:
+  * class LLNode {
+  *   constructor(value, next = null) {
+  *     this.value = value;
+  *     this.next = next;
+  *   }
+  * }
+  */
+function deleteMiddleNode(head) {
+  /* your code here */
+}
