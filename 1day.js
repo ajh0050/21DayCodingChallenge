8d38fffb8fc4085162545618133136f912058a12
Adam Hughes <adamjordanhughes@gmail.com>
2023-06-05 18:30:07 -0400

    Create class structure for the linked list node

--- 1day.js
+++ 1day.js
@@ -1,13 +1,9 @@
 
-/*
-  * You may assume the node class is:
-  * class LLNode {
-  *   constructor(value, next = null) {
-  *     this.value = value;
-  *     this.next = next;
-  *   }
-  * }
-  */
-function deleteMiddleNode(head) {
-  /* your code here */
+
+class LLNode {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
 }
+
