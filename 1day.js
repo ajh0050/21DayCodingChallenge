209635c060692722c477d2c60591251df7613df8
Adam Hughes <adamjordanhughes@gmail.com>
2023-06-05 18:32:03 -0400

    Create the solution for the delete method

--- 1day.js
+++ 1day.js
@@ -12,4 +12,23 @@ function deleteMiddleNode(head) {
         return head;
     }
 
+    let slow = head;
+    let fast = head;
+    let prev = null;
+
+    // We add an additional check for fast.next.next to make sure 
+    // that in case of an even length list, slow pointer points to the 
+    // first node of the second half of the list.
+    while (fast != null && fast.next != null && fast.next.next != null) {
+        fast = fast.next.next;
+        prev = slow;
+        slow = slow.next;
+    }
+
+    // delete the middle node
+    if (prev != null && slow != null) {
+        prev.next = slow.next;
+    }
+    
+    return head;
 }
